c2730c9f4fc369a1544224ed04493f3ab4bc9bcc
Aleks <alejandrodebian25@gmail.com>
2020-07-09 00:52:49 -0400

    creando un Layout por defecto. La ruta ,vista y controller para login

---
+++ application/views/login/login.view.php
@@ -0,0 +1,34 @@
+<div
+	class="container-fluid full d-flex align-items-center justify-content-center"
+>
+	<div class="row">
+		<div class="">
+			<div class="card">
+				<div class="card-header text-center">
+					Ingresar al sistema
+				</div>
+				<div class="card-body">
+					<form action="#" method="POST">
+						<div class="form-group">
+							<label for="ci">Usuario</label>
+							<input type="text" name="ci" id="ci" class="form-control" placeholder="Ingrese su CI" />
+						</div>
+						<div class="form-group">
+							<label for="clave">Password</label>
+							<input
+								type="password"
+								class="form-control"
+								id="clave"
+                                name="clave"
+                                placeholder="Ingrese su constraseña"
+							/>
+						</div>
+						<button type="submit" class="btn btn-block btn-primary">
+							Ingresar
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
